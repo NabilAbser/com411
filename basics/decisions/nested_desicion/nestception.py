0127bfcdbee8c14c2bb74fde10f5d9effa6d0ad8
Nabil <5nurun27@solent.ac.uk>
2021-10-07 10:53:33 +0100

    Added code to demonstrate the use of multiple nested statements.

---
+++ basics/decisions/nested_desicion/nestception.py
@@ -0,0 +1,5 @@
+print ("Where should I look?")
+room = input()
+
+if room == "in the bedroom":
+
---
+++ if_else.py
@@ -0,0 +1,9 @@
+print ("Print the activity to be performed")
+activity= input()
+if activity== "Calculate":
+    print("Performing calculations")
+
+else:
+    print("Performing activity")
+
+print ("Activity completed!")
